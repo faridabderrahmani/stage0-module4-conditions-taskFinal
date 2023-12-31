db75e9465b98854d41d82bc1d18583466119dcc0
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-31 09:25:30 +0100

    resolved

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,6 +1,12 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
-    public void swap(int first, int second) {
+    public static void main(String[] args) {
+
+    }
+    public  void swap(int first, int second) {
+        first = first^second;
+        second = first^second;
+        first = first^second;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,52 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if((year % 100 == 0 && year % 400 == 0 && year % 4 == 0)
+        || (year % 100 != 0 && year % 4 == 0)){
+            switch (month){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println(31);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println(30);
+                    break;
+                case 2:
+                    System.out.println(29);
+                    break;
+            }
+        }
+        else{
+                switch (month) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        System.out.println(31);
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        System.out.println(30);
+                        break;
+                    case 2:
+                        System.out.println(28);
+                        break;
+                    }
+                }
+        }
 
-    }
+
 }
