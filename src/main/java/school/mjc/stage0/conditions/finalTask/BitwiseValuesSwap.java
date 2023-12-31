0660306c8e986fa44564b51e94444e94fb7c91ae
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-31 10:44:24 +0100

    resolved

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public static void main(String[] args) {
-        swap(7, 5);
+
     }
-    public static void swap(int first, int second) {
+    public void swap(int first, int second) {
         first = first^second;
         second = first^second;
         first = first^second;
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public static void main(String[] args) {
-
+validate(0, 2, 5);
     }
-    public void validate(double firstSide, double secondSide, double thirdSide) {
+    public static void validate(double firstSide, double secondSide, double thirdSide) {
         if(firstSide!=0 && secondSide!=0 && thirdSide!=0){
             if(Math.abs(firstSide) + Math.abs(secondSide) > Math.abs(thirdSide)
                     && Math.abs(firstSide) + Math.abs(thirdSide) > Math.abs(secondSide)
@@ -14,6 +14,8 @@ public class TriangleSidesValidator {
             }else{
                 System.out.println("it's not a triangle");
             }
+        }else{
+            System.out.println("it's not a triangle");
         }
     }
 }
